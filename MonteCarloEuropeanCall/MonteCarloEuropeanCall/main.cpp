54d0cbcff5be31fabcd7688dbe05febd846ebc85
edijemeni <e.dijemeni@gmail.com>
2016-10-10 04:29:25 +0100

    Implementation of a simple european call

---
+++ MonteCarloEuropeanCall/MonteCarloEuropeanCall/main.cpp
@@ -0,0 +1,55 @@
+// maineq1.cpp
+//requires random.cpp
+
+#include "random.h"
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	cout << "\n *** START EQ1: Monte Carlo European Call *** \n";
+	// STEP 1: INTPUT PARAMETERS
+	double T = 1; // maturity
+	double K = 100; // strike
+	double S0 = 100; // spot
+	double sigma = 0.10; // volatility
+	double r = 0.05; // interest rate
+	int N = 500; // number of steps
+	int M = 10000; // number of simulations
+	double S[500+1]; // it doesnt like S[N+1]
+	double sumpayoff = 0;
+	double premium = 0;
+	double dt = T / N;
+
+	// STEP 2: MAIN SIMULATION LOOP
+	for (int j = 0; j < M; j++) {
+
+		S[0] = S0; // initialize each path for simulation
+
+		// STEP 3 : TIME INTEGRATION LOOP
+		for (int i = 0; i < N; i++) {
+			double epsilon = SampleBoxMuller(); //get Gaussian draw
+			S[i + 1] = S[i] * (1 + r*dt + sigma*sqrt(dt)*epsilon);
+		}
+
+		// STEP 4 : COMPUTE PAYOFF
+		sumpayoff += max(S[N] - K, 0.0); // compute and payoff
+	}
+
+	// STEP 5: COMPUTE DISCOUNTED EXPECTED PAYOFF
+	premium = exp(-r*T)*(sumpayoff / M);
+
+	// STEP 6: OUTPUT RESULTS
+
+	cout << "premium = " << premium << "\n";
+
+	cout << "\n *** END EQ1: Monte Carlo single asset *** \n";
+
+	_getch();
+	return 0;
+
+
+}
---
+++ MonteCarloEuropeanCall/MonteCarloEuropeanCall/random.h
@@ -0,0 +1,3 @@
+// Random.h
+
+double SampleBoxMuller(); 
